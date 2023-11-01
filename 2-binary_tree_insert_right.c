4d5c92bec18cffa501e01d3d1edfffcea5ddd1bf
Brig1tte <korantengbrigitte96@gmail.com>
2023-11-01 03:59:24 -0700

    B-Tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,8 +2,8 @@
 
 /**
  * *binary_tree_insert_right - funct to insert a node(right-child) of another
- * *parent: a pointer to the node to insert the right-child in
- * value: the value to store in the new node
+ * @parent: a pointer to the node to insert the right-child in
+ * @value: the value to store in the new node
  * Return: a pointer to the created node, NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
