bbcb4e22cc2a87b2364c5419865bda671a26ca26
Brig1tte <korantengbrigitte96@gmail.com>
2023-11-02 13:22:40 -0700

    B-Tree_Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,17 +7,16 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes = 0;
+	size_t leftNodes, rightNodes;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right != NULL)
-		nodes++;
-	else if (tree->left != NULL && tree->right == NULL)
-		nodes++;
-	else
-		return (tree->left + tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-	return (nodes);
+	leftNodes = binary_tree_nodes(tree->left);
+	rightNodes = binary_tree_nodes(tree->right);
+
+	return (1 + leftNodes + rightNodes);
 }
